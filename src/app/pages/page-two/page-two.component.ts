ac1ab25b611d6cdd7d8c55000bc4471065b5c5a8
Fatih Yıldırım <66695748+fthyildirim@users.noreply.github.com>
2023-02-08 04:32:21 +0300

    last commit

--- src/app/pages/page-two/page-two.component.ts
+++ src/app/pages/page-two/page-two.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MenuItem } from 'src/app/model/menu-item.model';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-page-two',
@@ -11,6 +13,7 @@ export class PageTwoComponent  {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private themeService: ThemeService,
   ) {
 
   }
@@ -23,10 +26,23 @@ export class PageTwoComponent  {
 
 
   submitForm() {
-    this.router.navigate(['/', 'flex', 'page-one'], { relativeTo: this.route })
+    this.router.navigate(['/', 'flex', 'page-two'], { relativeTo: this.route })
   }
- 
+
   get isResetDisable() {
     return !this.model.address && !this.model.title && !this.model.description;
+  }
+
+  get menuItemList() {
+    return this.themeService.themeList;
+  }
+
+  changeTheme(menuItem: MenuItem) {
+    this.themeService.changeTheme(menuItem);
+
+  }
+
+  get selectedMenuItem() {
+    return this.themeService.selectedMenuItem$.value;
   }
 }
